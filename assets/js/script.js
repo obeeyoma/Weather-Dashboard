6676c3918e2f7859a0d4b1775501d1d10f124fb6
Obioma <obeeomaa@gmail.com>
2024-01-25 12:24:04 +0100

    Add variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Variables for API url, search string , query
+  var apiURL = "https://api.openweathermap.org/data/2.5/forecast?";
+  var searchString = "";
+  var queryURL;
+
+  // Parent divs - today , forecast
+  var today = $("#today");
+  var forecast = $("#forecast");
+
+  // Variables for current day data
+  var cityName;
+  var todayDate;
+  var todayIcon;
+  var todayTemperature;
+  var todayWind;
+  var todayHumidity;
+
+  // Variables for forecast data
+  var forecastDate;
+  var forecastIcon;
+  var forecastTemperature;
+  var forecastWind;
+  var forecastHumidity;
+
+  // Array for results of data fetch
+  var results = [];
+});
