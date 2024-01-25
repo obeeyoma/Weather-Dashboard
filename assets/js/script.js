ee7ff430c7ca8709d17f13244957fffb947d9d07
Obioma <obeeomaa@gmail.com>
2024-01-25 12:47:01 +0100

    Implement functionality for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,29 @@ $(document).ready(function () {
   var modalTitle = $(".modal-title");
   var modalBodyText = $(".modal-body").children().eq(0);
 });
+
+// function to create button
+function createSearchedItemBtn(str) {
+  // check if the button with the same text content already exists and append it if not
+  var searchedItemList = $("#history");
+  var searchedItemBtns = searchedItemList.find("button");
+
+  var buttonExists =
+    searchedItemBtns.filter(function () {
+      return $(this).text() === str;
+    }).length > 0;
+
+  if (!buttonExists) {
+    // Create button
+    var searchedItemBtn = $("<button>");
+    searchedItemBtn.addClass("btn btn-secondary search-button mt-1");
+    searchedItemBtn.text(str).addClass("text-capitalize");
+    searchedItemList.prepend(searchedItemBtn);
+  } else {
+    return;
+  }
+}
+
 // Click button to hide modal
 $("#close-btn").on("click", function () {
   hideModal();
